aa6b140a43bfad60960c5b5d71415a51c1f8b658
Hassam Ul Haq <hassam1958a@gmail.com>
2024-03-19 04:45:40 +0500

    Initial commit

---
+++ main.ts
@@ -0,0 +1,50 @@
+// object
+let boy = {
+    firstName: "Irfan",
+    lastName: "Rajput",
+    age: 28,
+    gender: 'male'
+}
+
+console.log(boy)
+
+let student = {
+    fullName: 'Haris',
+    classId: 'A332',
+    rollNo: 32,
+    email: 'haris@gmail.com',
+    isPassed: true,
+    subjects: ['Computer Science', 'Mathmetics', 'Physics'],
+}
+
+let student2 = {
+    fullName: 'Motasim',
+    classId: 'A333',
+    rollNo: 33,
+    email: 'motasim@gmail.com',
+    isPassed: true,
+    subjects: ['Computer Science', 'Mathmetics', 'Physics'],
+}
+let student3 = {
+    fullName: 'Wasif',
+    classId: 'A334',
+    rollNo: 34,
+    email: 'wasif@gmail.com',
+    isPassed: true,
+    subjects: ['Computer Science', 'Mathmetics', 'Physics'],
+}
+
+let students = [student, student2, student3]
+console.log(students)
+
+// Array of object
+let information = {
+    fullName: 'Raja Atif',
+    ClassId: "RA07",
+    rollNo: 7,
+    email: 'raja07@gmail.com',
+    isPassed: true,
+    string: ['Computer Science', 'Mathametics', "Physics"]
+}
+
+console.log(information)
